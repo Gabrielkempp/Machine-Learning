1e180c1748f944902a504fa93f6fb7c0c082a4f9
Gabriel Kempp <gabrielrkempp@gmail.com>
2024-07-22 22:48:25 -0300

    Commit Inicial

---
+++ openCsv[credit_data].py
@@ -0,0 +1,19 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+import plotly.express as px
+
+base_credit = pd.read_csv("Data/credit_data.csv")
+
+'''
+
+print(base_credit) # defalted
+print(base_credit.head(2)) # Busca os Primeiros registors na base de dados
+print(base_credit.head(2)) # Busca os Ultimos registro na base de dados
+print(base_credit.describe()) # Faz uma descrição completa da tabela
+print(base_credit[base_credit["income"] >= 69995.685578 ]) # Mostra quem é que possui essa receita
+print(base_credit[base_credit["loan"] <= 1.40 ]) # Mostra quem que possui esta divida
+
+'''
+print(base_credit[base_credit["loan"] <= 1.40 ])
